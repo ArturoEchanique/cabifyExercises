ff65f312b0594e04cc1d50e6175de2020207703f
Arturo Echanique <arturoechanique@gmail.com>
2022-06-02 12:07:59 +0200

    update messages route error management

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -10,28 +10,34 @@ router.get("/hello-world", (req, res) => {
 
 router.post("/messages", (req, res) => {
 
-    const { destination, message } = req.body
 
-    if (!req.body.destination) {
-        res.status(400).json({ message: 'Destination field is required' })
-    } else if (!req.body.message) {
-        res.status(400).json({ message: 'Message field is required' })
-    } else if (checkIfEmpty(destination) && checkIfEmpty(message)) {
-        res.status(400).json({ message: 'Destination and message fields are required' })
-    } else if (destination && checkIfEmpty(destination)) {
-        res.status(400).json({ message: 'Destination field is required' })
-    } else if (message && checkIfEmpty(message)) {
-        res.status(400).json({ message: 'Message field is required' })
-    } else if (Object.keys(req.body).length > 2) {
-        console.log("object is", Object.keys(req.body))
-        res.status(400).json({ message: 'Payload must not contain keys different to _destination_ and _message_' })
-    } else {
-        messagesService
-            .sendMessage({ destination, message })
-            .then(response => { res.status(200).json({ message: "Message sent", response: response.data }) })
-            .catch(err => res.status(500).json({ message: "The message could not be sent" }))
+    if (!('destination' in req.body)) {
+        res.status(400).json({ message: 'Destination key is required' })
     }
-});
+    else if (!('message' in req.body)) {
+        res.status(400).json({ message: 'Message key is required' })
+    }
+
+    else {
+
+        const { destination, message } = req.body
+        
+        if (checkIfEmpty(destination) && checkIfEmpty(message)) {
+            res.status(422).json({ message: 'Destination and message fields are required' })
+        } else if (checkIfEmpty(destination)) {
+            res.status(422).json({ message: 'Destination field is required' })
+        } else if (checkIfEmpty(message)) {
+            res.status(422).json({ message: 'Message field is required' })
+        } else if (Object.keys(req.body).length > 2) {
+            res.status(413).json({ message: 'Payload must not contain keys different to _destination_ and _message_' })
+        } else {
+            messagesService
+                .sendMessage({ destination, message })
+                .then(response => { res.status(200).json({ message: "Message sent", response: response.data }) })
+                .catch(err => res.status(500).json({ message: "The message could not be sent" }))
+        }
+    }
+})
 
 
 module.exports = router
