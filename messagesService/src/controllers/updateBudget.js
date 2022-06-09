e75461ff7ec4233c10bfc981ab2ef06a91c41a24
Arturo Echanique <arturoechanique@gmail.com>
2022-06-09 22:18:17 +0200

    code cleaning

--- messagesService/src/controllers/updateBudget.js
+++
@@ -1,7 +0,0 @@
-import updateBudget from "../clients/updateBudget.js";
-
-export default async (req, res) => {
-    const {amount} = req.body
-    const budget = await updateBudget(amount);
-    res.json(budget);
-}
--- messagesService/src/repDatabase.js
+++ messagesService/src/repDatabase.js
@@ -7,20 +7,3 @@ const database = "cabify_bootcamp";
 export default mongoose.createConnection(`mongodb://${server}/${database}`, {
   useNewUrlParser: true,
 });
-
-// export default async function () {
-//   return new Promise((resolve, reject) => {
-//     let conn = mongoose.createConnection(`mongodb://${server}/${database}`, {
-//       useNewUrlParser: true,
-//     })
-//     conn.on("error", function (err) {
-//       reject(err);
-//     })
-//     conn.on("disconnected", function (err) {
-//       reject(err);
-//     })
-//     conn.on("connected", function () {
-//       resolve(conn);
-//     })
-//   })
-// };
